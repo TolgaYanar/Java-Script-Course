72ce548fcf015592ea229a415e54f6a22c41079d
Tolga Yanar <tolga.yanar@ug.bilkent.edu.tr>
2025-09-30 13:36:37 +0300

    Arrays added

---
+++ coding-challange-17.js
@@ -0,0 +1,23 @@
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice(1, -2);
+  const allDogs = dogsJuliaCorrected.concat(dogsKate);
+  allDogs.forEach(function (dog, index) {
+    if (dog < 3) {
+      console.log(`Dog number ${index + 1} is still a puppy 🐶`);
+    } else {
+      console.log(
+        `Dog number ${index + 1} is an adult, and is ${dog} years old 🐕`
+      );
+    }
+  });
+};
+
+// Test Data 1
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+checkDogs(dogsJulia, dogsKate);
+
+// Test Data 2
+const dogsJulia2 = [9, 16, 6, 8, 3];
+const dogsKate2 = [10, 5, 6, 1, 4];
+checkDogs(dogsJulia2, dogsKate2);
